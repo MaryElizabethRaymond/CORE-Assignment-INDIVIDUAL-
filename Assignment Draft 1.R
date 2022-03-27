445462ce2b6ea6692999db5f6f7c3ddda8d86864
MaryElizabethRaymond <98770156+MaryElizabethRaymond@users.noreply.github.com>
2022-03-27 17:04:48 -0400

    Add files via upload

--- Assignment Draft 1.R
+++ Assignment Draft 1.R
@@ -1,47 +1,40 @@
-library(readr)
-library(dplyr)
+#Part A Python Script 
+outFile=open('./MyFileName.seq','w+')
+import random
+Nb=30
+dna = ["A","G","C","T"]
+#initialise empty string
+#this is where the bases will be added as they are generated
+random_sequence=''
+# We'll create a string of 100 random bases
+for i in range(Nb):
+  random_sequence+=random.choice(dna)
+outFile.write(random_sequence)
+outFile.close()
 
-# split it into a vector of individual characters:
-strsplit(myseq, "")[[1]]
-#  [1] "A" "G" "C" "T" "T" "T" "T" "C" "A" "T" "T" "C" "T" "G" "A" "C" "T" "G" "C" "A" "A" "C" "G" "G" "G" "C" "A" "A" "T" "A" "T" "G" "T" "C" "T" "C" "T" "G" "T"
-# [40] "G" "T" "G" "G" "A" "T" "T" "A" "A" "A" "A" "A" "A" "A" "G" "A" "G" "T" "G" "T" "C" "T" "G" "A" "T" "A" "G" "C" "A" "G" "C"
-
-# count the frequencies of each
-table(strsplit(myseq, ",")[[1]])
-# A  C  G  T 
-# 20 12 17 21 
-strsplit(as.character(TestGenome), split="", fixed=T)
-
-TestGenome2<-read.csv("./Fasta.txt")
-print(TestGenome)
-paste(TestGenome2
-    )
-strsplit(as.character(TestGenome2), split="", fixed=T)
-#B
-library("seqinr")
-TestGenome<-read.fasta("./GCR2_Genome.fsa")
-seq<-paste(TestGenome[1])
-count(seq,[["a"]])
-
-seq2<-c("A","T","G","C","T")
-length(seq2)
+inFile = open('./MyFileName.seq', 'r')
+contents = inFile.read()
+print(contents)
 
 
+#Part B
+
+library("seqinr")                       
 DNA<-read.fasta("./FileName.seq")
 seq<-paste(DNA[1])
 seqtable<-count(seq,1)
-BASE.R<-function(seq){
+BASE.R<-function(seqtable){
   A<-seqtable[["A"]]
   C<-seqtable[["C"]]
   T<-seqtable[["T"]]
   G<-seqtable[["G"]]
-  outlist<-list()
+  outlist<-list(A,C,T,G)
 }
 
 
 
 #Part C 
-Numbers<-read.csv("./Numbers.csv")
+Numbers<-read.csv("./FileName.csv")
 Numbers$var1<-Numbers$var1/mean(Numbers$var1)
 Numbers$var2<-Numbers$var2/mean(Numbers$var2)
 Numbers$var3<-Numbers$var3/mean(Numbers$var3)
