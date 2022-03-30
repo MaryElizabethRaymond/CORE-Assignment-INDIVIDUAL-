ff84d33e258e2adc089c27c9fc5bbacf314fd852
MaryElizabethRaymond <98770156+MaryElizabethRaymond@users.noreply.github.com>
2022-03-30 19:58:55 -0400

    Add files via upload

---
+++ DNAgen.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+outFile=open('./MyFileName.seq','w+')
+import random
+Nb=100
+dna = ["A","G","C","T"]
+random_sequence=''
+for i in range(Nb):
+  random_sequence+=random.choice(dna)
+outFile.write(random_sequence)
+outFile.close()
